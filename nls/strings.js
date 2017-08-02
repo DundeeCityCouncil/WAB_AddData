be3a0d49ce18ad65320712d1c9a496660bd04ae2
CWRahien <CWRahien@users.noreply.github.com>
2017-08-02 10:18:29 +0100

    first commit

---
+++ nls/strings.js
@@ -0,0 +1,177 @@
+define({
+  root: ({
+    _widgetLabel: "Add Data",
+
+    noOptionsConfigured: "No options were configured.",
+
+    tabs: {
+      search: "Search",
+      url: "URL",
+      file: "File"
+    },
+
+    search: {
+      featureLayerTitlePattern: "{serviceName} - {layerName}",
+      layerInaccessible: "The layer is inaccessible.",
+      loadError: "AddData, unable to load:",
+      searchBox: {
+        search: "Search",
+        placeholder: "Search..."
+      },
+      bboxOption: {
+        bbox: "Within map"
+      },
+      scopeOptions: {
+        anonymousContent: "Content",
+        myContent: "My Content",
+        myOrganization: "My Organization",
+        curated: "Curated",
+        ArcGISOnline: "ArcGIS Online"
+      },
+      sortOptions: {
+        prompt: "Sort By:",
+        relevance: "Relevance",
+        title: "Title",
+        owner: "Owner",
+        rating: "Rating",
+        views: "Views",
+        date: "Date",
+        switchOrder: "Switch"
+      },
+      typeOptions: {
+        prompt: "Type",
+        mapService: "Map Service",
+        featureService: "Feature Service",
+        imageService: "Image Service",
+        vectorTileService: "Vector Tile Service",
+        kml: "KML",
+        wms: "WMS"
+      },
+      resultsPane: {
+        noMatch: "No results were found."
+      },
+      paging: {
+        first: "<<",
+        firstTip: "First",
+        previous: "<",
+        previousTip: "Previous",
+        next: ">",
+        nextTip: "Next",
+        pagePattern: "{page}"
+      },
+      resultCount: {
+        countPattern: "{count} {type}",
+        itemSingular: "Item",
+        itemPlural: "Items"
+      },
+
+      item: {
+        actions: {
+          add: "Add",
+          close: "Close",
+          remove: "Remove",
+          details: "Details",
+          done: "Done",
+          editName: "Edit Name"
+        },
+        messages: {
+          adding: "Adding...",
+          removing: "Removing...",
+          added: "Added",
+          addFailed: "Add failed",
+          unsupported: "Unsupported"
+        },
+        typeByOwnerPattern: "{type} by {owner}",
+        dateFormat: "MMMM d, yyyy",
+        datePattern: "{date}",
+        ratingsCommentsViewsPattern: "{ratings} {ratingsIcon} {comments} {commentsIcon} {views} {viewsIcon}",
+        ratingsCommentsViewsLabels: {"ratings": "ratings", "comments": "comments", "views": "views"},
+        types: {
+          "Map Service": "Map Service",
+          "Feature Service": "Feature Service",
+          "Image Service": "Image Service",
+          "Vector Tile Service": "Vector Tile Service",
+          "WMS": "WMS",
+          "KML": "KML"
+        }
+      }
+    },
+
+    addFromUrl: {
+      type: "Type",
+      url: "URL",
+      types: {
+        "ArcGIS": "An ArcGIS Server Web Service",
+        "WMS": "A WMS OGC Web Service",
+        "WMTS": "A WMTS OGC Web Service",
+        "WFS": "A WFS OGC Web Service",
+        "KML": "A KML File",
+        "GeoRSS": "A GeoRSS File",
+        "CSV": "A CSV File"
+      },
+      samplesHint: "Sample URL(s)"
+    },
+
+    addFromFile: {
+      intro: "You can drop or browse for one the following file types:",
+      types: {
+        "Shapefile": "A Shapefile (.zip, ZIP archive containing all shapefile files)",
+        "CSV": "A CSV File (.csv, with address or latitude, longitude and comma, semi-colon or tab delimited)",
+        "GPX": "A GPX File (.gpx, GPS Exchange Format)",
+        "GeoJSON": "A GeoJSON File (.geo.json or .geojson)"
+      },
+      generalizeOn: "Generalize features for web display",
+      dropOrBrowse: "Drop or Browse",
+      browse: "Browse",
+      invalidType: "This file type is not supported.",
+      addingPattern: "{filename}: adding...",
+      addFailedPattern: "{filename}: add failed",
+      featureCountPattern: "{filename}: {count} feature(s)",
+      invalidTypePattern: "{filename}: this type is not supported",
+      maxFeaturesAllowedPattern: "A maximum of {count} features is allowed",
+      layerNamePattern: "{filename} - {name}"
+    },
+
+    layerList: {
+      caption: "Layers",
+      noLayersAdded: "No layers have been added.",
+      removeLayer: "Remove Layer",
+      back: "Back"
+    }
+
+  }),
+  "ar": 0,
+  "bs": 0,
+  "cs": 0,
+  "da": 0,
+  "de": 0,
+  "el": 0,
+  "es": 1,
+  "et": 0,
+  "fi": 0,
+  "fr": 0,
+  "he": 0,
+  "hi": 0,
+  "hr": 0,
+  "it": 0,
+  "id": 0,
+  "ja": 0,
+  "ko": 0,
+  "lt": 0,
+  "lv": 0,
+  "nb": 0,
+  "nl": 0,
+  "pl": 0,
+  "pt-br": 0,
+  "pt-pt": 0,
+  "ro": 0,
+  "ru": 0,
+  "sr": 0,
+  "sv": 0,
+  "th": 0,
+  "tr": 0,
+  "vi": 0,
+  "zh-cn": 0,
+  "zh-hk": 0,
+  "zh-tw": 0
+});
---
+++ search/AddFromGISPane.js
@@ -0,0 +1,139 @@
+define(['dojo/_base/declare',
+    'jimu/BaseWidget',
+    'jimu/loaderplugins/jquery-loader!https://code.jquery.com/jquery-1.11.2.min.js',
+    'esri/arcgis/Portal',
+    'esri/arcgis/utils',
+    "./LayerLoader",
+    "dijit/_TemplatedMixin",
+    "dijit/_WidgetsInTemplateMixin",
+    "dojo/text!./templates/AddFromGISPane.html",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    "esri/layers/ArcGISImageServiceLayer",
+    "esri/layers/ArcGISTiledMapServiceLayer",
+    "esri/layers/DynamicLayerInfo",
+    "esri/layers/FeatureLayer",
+    "esri/layers/ImageParameters",
+    "esri/layers/ImageServiceParameters",
+    "esri/layers/KMLLayer",
+    "esri/layers/LayerDrawingOptions",
+    "esri/layers/MosaicRule",
+    "esri/layers/RasterFunction",
+    "esri/layers/VectorTileLayer",
+    'esri/layers/WMSLayer', 'esri/dijit/PopupTemplate'],
+        function (declare, BaseWidget, $, arcgisPortal, arcgisUtils, LayerLoader, _TemplatedMixin, _WidgetsInTemplateMixin,template, ArcGISDynamicMapServiceLayer, ArcGISImageServiceLayer, ArcGISTiledMapServiceLayer,
+                DynamicLayerInfo, FeatureLayer, ImageParameters, ImageServiceParameters, KMLLayer,
+                LayerDrawingOptions, MosaicRule, RasterFunction, VectorTileLayer, WMSLayer, PopupTemplate) {
+            //To create a widget, you need to derive from BaseWidget.
+
+            return declare([BaseWidget, _TemplatedMixin, _WidgetsInTemplateMixin], {
+                // Custom widget code goes here
+
+                baseClass: 'jimu-widget-addmapdata',
+                templateString: template,
+                wabWidget: null,
+
+                //this property is set by the framework when widget is loaded.
+                //name: 'AddMapData',
+
+
+                //methods to communication with app container:
+
+                postCreate: function () {
+                    this.inherited(arguments);
+
+                },
+
+                startup: function () {
+
+                    this.inherited(arguments);
+                    this.portalQuery();
+                },
+
+                _portalMap: {},
+                portalQuery: function () {
+                    var data = this;
+                    var OpLayers = {};
+                    var myPortal = new arcgisPortal.Portal("https://mapsonline.dundeecity.gov.uk/portal/");
+                    var params = {q: "group:\"574abc27ed8a475faeba54d9a0bfd9ed\" type:\"Web Map\"", num: 100};
+                    myPortal.queryItems(params).then(function (groups) {
+                        for (var result in groups.results)
+                        {
+                            var layerTitle = groups.results[result].title;
+                            var MapID = groups.results[result].id;
+                            data.mapItems.innerHTML += "<li data-portal-item='" + MapID + "'>" + layerTitle + "</li>";
+                            arcgisUtils.getItem(MapID).then(function (eachLayer) {
+                                opTitle = eachLayer.item.title;
+                                OpLayers[opTitle] = eachLayer;
+                                data._portalMap[eachLayer.item.id] = eachLayer;
+                            });
+                        }
+                    });
+                },
+
+                loadLayer: function (event) {
+                    
+                    this.inherited(arguments);
+                    
+                    var dataL = this.wabWidget;
+                    
+                   
+                    var ID = $(event.target).data('portal-item');
+                    var tempMapObj = arcgisUtils.createMap(ID, "map2");
+                    tempMapObj.then(function (response) {
+                        var tempMap = response.map;
+                        for (var j = 0; j < tempMap.graphicsLayerIds.length; j++)
+                        {
+
+                            var tempGLayer = tempMap.getLayer(tempMap.graphicsLayerIds[j]);
+                            dataL.map.addLayer(tempGLayer);
+                        }
+
+                        for (var i = 1; i < tempMap.layerIds.length; i++)
+                        {
+                            //This loops skips the first object (Basemap)
+                            var tempFLayer = tempMap.getLayer(tempMap.layerIds[i]);
+                            dataL.map.addLayer(tempFLayer);
+
+                        }
+
+                    });
+                }
+
+
+                // onOpen: function(){
+                //   console.log('onOpen');
+                // },
+
+                // onClose: function(){
+                //   console.log('onClose');
+                // },
+
+                // onMinimize: function(){
+                //   console.log('onMinimize');
+                // },
+
+                // onMaximize: function(){
+                //   console.log('onMaximize');
+                // },
+
+                // onSignIn: function(credential){
+                //   /* jshint unused:false*/
+                //   console.log('onSignIn');
+                // },
+
+                // onSignOut: function(){
+                //   console.log('onSignOut');
+                // }
+
+                // onPositionChange: function(){
+                //   console.log('onPositionChange');
+                // },
+
+                // resize: function(){
+                //   console.log('resize');
+                // }
+
+                //methods to communication between widgets:
+
+            });
+        });
